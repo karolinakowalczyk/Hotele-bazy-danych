c9019e1a21a141f7c640f6ae4056bd2fd7bd40d0
Karolina <karolinakowalczyk1999@gmail.com>
2021-01-05 21:09:04 +0100

    podłączenie bazy pod Django

--- hotele_baza/hotele_baza/hotele_baza/settings.py
+++ hotele_baza/hotele_baza/hotele_baza/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'hotele_baza.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE' : 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER' : 'root',
+        'PASSWORD' : '',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
