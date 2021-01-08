707249a8ad18526bf4cc02d55b52c92c2806b4a1
Kilof99 <37956053+Kilof99@users.noreply.github.com>
2021-01-08 19:14:58 +0100

    .gitignore is now working?

--- hotele_baza/hotele_baza/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Karolina\source\repos\karolinakowalczyk\Hotele-bazy-danych\hotele_baza\hotele_baza\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
