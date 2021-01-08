62bf575447f31c442af2bfa5d62f5029d31f1947
Karolina <karolinakowalczyk1999@gmail.com>
2021-01-08 22:46:12 +0100

    Sign up WORK :D

---
+++ hotele_baza/hotele_baza/databaseapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Users
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = Users
+        fields = ['login', 'password','email', 'phone', 'name', 'surname']
+        labels = {'login': 'login', 'password': 'password', 'email': 'email', 'phone': 'phone', 'name': 'name', 'surname': 'surname'}
+        widgets = {
+            'password': forms.PasswordInput,
+            'email': forms.EmailInput,
+        }
--- hotele_baza/hotele_baza/databaseapp/views.py
+++ hotele_baza/hotele_baza/databaseapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+#from django.contrib.auth.forms import UserCreationForm
 from .models import Users, Rooms
+from .forms import SignUpForm
 
 def index(request):
     user_list = Users.objects.order_by('surname')[:5]
@@ -10,7 +12,15 @@ def login(request):
     return render(request, 'databaseapp/login.html')
 
 def signUp(request):
-    return render(request, 'databaseapp/signUp.html')
+    if request.method != 'POST':
+        form = SignUpForm()
+    else:
+        form = SignUpForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('databaseapp:index')
+    context = {'form': form}
+    return render(request, 'databaseapp/signUp.html', context)
 
 def browse(request):
     rooms_list = Rooms.objects.order_by('room_id')
